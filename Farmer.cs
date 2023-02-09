6abbc6135ae5f7a8a21708324f5f01e733fbc3e6
Rasmusbb <56928150+Rasmusbb@users.noreply.github.com>
2023-02-09 14:28:38 +0100

    Hello2

--- Farmer.cs
+++ Farmer.cs
@@ -3,15 +3,18 @@ using System;
 
 public class Farmer : Area2D
 {
-    int speed = 200;
+    int speed = 20;
     public bool alive = true;
     public Poop poop;
 
     float delay = 0;
     Player chicken;
     Vector2 chickenPos;
+    [Export]
     float newrotation = 0;
-
+    public static float uirotation = 0;
+    public static float uiangle = 0;
+    public static float uidiff = 0;
     float turnspeed = 3.00f;
     public override void _Ready()
     {
@@ -21,28 +24,35 @@ public class Farmer : Area2D
 
     public override void _Process(float delta)
     {
+        float dif = 0;
         chickenPos = GetGlobalMousePosition();
-        //GD.Print(chickenPos);
         //GD.Print(Position);
-        float dif = newrotation - Position.AngleToPoint(chickenPos);
-        if (dif > Mathf.Tau)
-            dif -= Mathf.Tau;
-        else if (dif < Mathf.Tau)
-        {
-            dif += Mathf.Tau;
-        }
-        //GD.Print(Position.AngleToPoint(chickenPos));
-        if (Input.IsActionPressed("move_up"))
-        {
-            newrotation += delta * turnspeed;
-        }
-        else if (Input.IsActionPressed("move_down"))
-        {
-            newrotation -= (delta * turnspeed);
-        }
+        dif = Position.AngleToPoint(chickenPos) - newrotation;
+        uirotation = newrotation;
+        uiangle = Position.AngleToPoint(chickenPos);
+        uidiff = dif;
+        // if (dif < Mathf.Tau)
+        // {
+        //     dif -= Mathf.Tau;
+        // }
+        // else if (dif > -Mathf.Tau)
+        // {
 
-        GD.Print(newrotation);
-        Vector2 dir = Vector2.Right.Rotated(newrotation);
+        //     dif += Mathf.Tau;
+        // }
+        //GD.Print(Position.AngleToPoint(chickenPos));
+        newrotation += Position.AngleToPoint(chickenPos) * delta * turnspeed;
+
+        // if (Position.AngleToPoint(chickenPos) > newrotation)
+        // {
+        //     newrotation += delta * turnspeed;
+        // }
+        // else if (Position.AngleToPoint(chickenPos) < newrotation)
+        // {
+        //     newrotation -= (delta * turnspeed);
+        // }
+
+        Vector2 dir = Vector2.Up.Rotated(newrotation);
 
         Position += dir * speed * delta;
 
--- UI.cs
+++ UI.cs
@@ -4,13 +4,23 @@ using System;
 public class UI : Node
 {
     Label scoreLabel;
+    Label var1;
+    Label var2;
+    Label var3;
     public override void _Ready()
     {
         scoreLabel = GetNode<Label>("scoreLabel");
+        var1 = GetNode<Label>("var1");
+        var2 = GetNode<Label>("var2");
+        var3 = GetNode<Label>("var3");
     }
 
     public override void _Process(float delta)
     {
         scoreLabel.Text = Main.score.ToString();
+        var1.Text = Farmer.uirotation.ToString();
+        var2.Text = Farmer.uiangle.ToString();
+        var3.Text = Farmer.uidiff.ToString();
+
     }
 }
